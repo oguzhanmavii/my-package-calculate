ed51252f3895e180722c5675bfa24793f41fc771
OĞUZHAN MAVİ <77650437+oguzhanmavii@users.noreply.github.com>
2022-09-05 12:53:39 +0300

    first commit

---
+++ build/lib/calculate/__init__.py
@@ -0,0 +1,4 @@
+from . calculate import MainLoop
+from . calculate import Menu
+from . calculate import MenuLoop
+from . calculate import Multiplication
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='calculateapp',
+    version='0.1',
+    author='Oguzhan Mavi',
+    author_email='oguzhanmavii98@gmail.com',
+    description='bu paket test etmek icin kurulmus bir hesap makinesidird',
+    py_modules=['calculate'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+    [console_scripts]
+    calculate=calculate:Mainloop
+    ''',
+    packages=['calculate'],
+)
